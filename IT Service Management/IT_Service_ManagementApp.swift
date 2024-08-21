54b6dd391278b016255062400485f2dcbf8fc966
Nick Black <nickblack@VELO-V201849.local>
2024-08-21 09:51:05 -0500

    Initial Commit

---
+++ IT Service Management/IT_Service_ManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  IT_Service_ManagementApp.swift
+//  IT Service Management
+//
+//  Created by Nick Black on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct IT_Service_ManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
